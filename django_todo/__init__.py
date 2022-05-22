70d5aa51f8a00ca1fab38bc3f74cd1254b378849
Karol Mileszko <karol.miles@gmail.com>
2022-05-22 13:52:13 +0000

    Prepare test files

--- django_todo/__init__.py
+++ django_todo/__init__.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
+# my addition to remove errors in terminal / karol
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 from django_todo import routes  # noqa
