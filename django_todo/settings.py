a91fd091c13b5a1aafafc1f53241c978d617e89f
Karol Mileszko <karol.miles@gmail.com>
2022-05-30 09:53:38 +0000

    Clean settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,15 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import sys
-
-# Karol: hiding SECRET_KEY
-
-# append the path of the parent directory
-sys.path.append(".")
-
-if os.path.isfile('env.py'):
-    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
