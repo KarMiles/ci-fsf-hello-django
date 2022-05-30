a0bc393a64e4aeef38fce2f8dffe2510e45f5aeb
Karol Mileszko <karol.miles@gmail.com>
2022-05-30 10:29:32 +0000

    Set up environment variables

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-todo-app72.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
